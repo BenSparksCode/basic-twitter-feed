0a60b82b3101efc7fef6c0cc3904575104fa4a43
Ben Sparks <bensparks0@gmail.com>
2021-03-11 16:45:06 +0200

    Server pulls text successfully from user.txt and serves to /users

--- server/index.js
+++ server/index.js
@@ -1,11 +1,42 @@
 const express = require('express')
+const fs = require('fs')
+var path = require('path');
+
+// const userFile = require('../data/user.txt')
+const tweetFile = ''
+
 const app = express()
 const port = 5000
 
+app.get('/users', (req, res) => {
+
+    let data = ''
+
+    const readStream = fs.createReadStream(
+        path.join(__dirname, '../data') + '/user.txt',
+        'utf8');
+
+    readStream.on('data', function (chunk) {
+        if(chunk) data += chunk;
+    }).on('end', function () {
+        console.log(data);
+
+        // process user follows string
+        let follows = data.split("\n")
+
+        const users = {
+            follows: follows
+        }
+
+        return res.status(201).json(users)
+    });
+})
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
