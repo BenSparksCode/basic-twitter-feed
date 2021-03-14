bbfb36fdfbc48a3318b61b91a19e2653e37d3a40
Ben Sparks <bensparks0@gmail.com>
2021-03-14 22:18:58 +0200

    Added loading indicator and logic to Feed component

--- app/src/components/Feed.js
+++ app/src/components/Feed.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useContext } from 'react'
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
-import {CardContainer} from './Card'
+import { AppContext } from '../contexts/AppContext'
+
+import { CardContainer } from './Card'
+import { Tweet } from './Tweet'
+
+const loadingIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
 export const Feed = () => {
+
+    const { userFeed, userFeedLoaded } = useContext(AppContext)
+
+
     return (
         <CardContainer>
-            Feed
+            {
+                !userFeedLoaded
+                    ?
+                    <p><Spin className='StatsIndicator' indicator={loadingIcon} /> Loading Tweets</p>
+                    :
+                    userFeed?.length > 0
+                        ?
+                        userFeed.map(tweet => <Tweet tweetData={tweet} />)
+                        :
+                        <p>No tweets yet...</p>
+            }
         </CardContainer>
     )
 }
